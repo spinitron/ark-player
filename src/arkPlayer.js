6cd2a29fc3163ad10af21b3470ba34c835c40d66
Tom Worster <tom@spinitron.com>
2021-12-22 20:28:17 -0500

    display Native or Hls.js

--- src/arkPlayer.js
+++ src/arkPlayer.js
@@ -179,11 +179,13 @@
                     pressPlay();
                 });
             });
+            document.querySelector('.ark-player__mode-hint').innerHTML = 'H';
         } else if (theMediaElement.canPlayType('application/vnd.apple.mpegurl')) {
             debug("Using native HLS");
             theMediaElement.src = playlistUrl;
             pleaseReload = false;
             pressPlay();
+            document.querySelector('.ark-player__mode-hint').innerHTML = 'N';
         }
     }
 
